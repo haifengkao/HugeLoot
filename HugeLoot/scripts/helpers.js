d855a235a326419d6b58a3a9bb6ff56107761e38
Hai Feng Kao <haifeng@cocoaspice.in>
2021-04-20 13:58:57 +0800

    correct the HugeLoot product path

--- HugeLoot/scripts/helpers.js
+++ HugeLoot/scripts/helpers.js
@@ -6,63 +6,80 @@ const fs = require('fs');
 const os = require('os');
 
 function fileExists(filePath) {
-  try {
-    // Query the entry
-    const stats = fs.lstatSync(filePath);
+    try {
+        // Query the entry
+        const stats = fs.lstatSync(filePath);
 
-    // Is it a directory?
-    if (stats.isFile()) {
-      return true;
+        if (os.platform() === 'darwin') {
+            // xxx.app is a folder in osx
+            if (stats.isDirectory()) {
+                return true
+            }
+
+        } else if (stats.isFile()) {
+            // Is it a directory?
+            return true;
+        }
+    } catch (e) {
+        /* Don't do anything, it's not an error. */
     }
-  } catch (e) {
-    /* Don't do anything, it's not an error. */
-  }
 
-  return false;
+    return false;
 }
 
 function getBinaryParentPaths(rootPath) {
-  const paths = [
-    {
-      path: path.join(rootPath, 'build'),
-      label: null
-    },
-    {
-      path: path.join(rootPath, 'build', '32'),
-      label: '32-bit'
-    },
-    {
-      path: path.join(rootPath, 'build', '64'),
-      label: '64-bit'
+    const paths = [
+        {
+            path: path.join(rootPath, 'release'),
+            label: null
+        },
+        {
+            path: path.join(rootPath, 'debug'),
+            label: null
+        },
+        {
+            path: path.join(rootPath, 'build'),
+            label: null
+        },
+        {
+            path: path.join(rootPath, 'build', '32'),
+            label: '32-bit'
+        },
+        {
+            path: path.join(rootPath, 'build', '64'),
+            label: '64-bit'
+        }
+    ];
+
+    if (os.platform() === 'win32') {
+        paths.forEach(parentPath => {
+            parentPath.path = path.join(parentPath.path, 'Release');
+        });
     }
-  ];
 
-  if (os.platform() === 'win32') {
-    paths.forEach(parentPath => {
-      parentPath.path = path.join(parentPath.path, 'Release');
-    });
-  }
-
-  return paths;
+    return paths;
 }
 
 function getAppReleasePaths(rootPath) {
-  let file = 'LOOT';
-  if (os.platform() === 'win32') {
-    file += '.exe';
-  }
+    let file = 'HugeLoot';
+    if (os.platform() === 'win32') {
+        file += '.exe';
+    }
+    if (os.platform() === 'darwin') {
+        file += '.app';
+    }
 
-  return getBinaryParentPaths(rootPath).filter(parentPath =>
-    fileExists(path.join(parentPath.path, file))
-  );
+    return getBinaryParentPaths(rootPath).filter(parentPath =>
+        fileExists(path.join(parentPath.path, file))
+    );
 }
 
 function safeExecFileSync(file, args, options) {
-  try {
-    return childProcess.execFileSync(file, args, options);
-  } catch (error) {
-    throw new Error(error.message);
-  }
+    try {
+        return childProcess.execFileSync(file, args, options);
+    } catch (error) {
+        throw new Error(error.message);
+    }
 }
 
 module.exports.fileExists = fileExists;
